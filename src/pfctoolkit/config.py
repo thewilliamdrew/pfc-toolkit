215e916a6dd1b949e308612414f818b32c44f319
William Drew <william.drew100@gmail.com>
2021-12-25 23:57:03 -0500

    added config.py

---
+++ src/pfctoolkit/config.py
@@ -0,0 +1,26 @@
+import os, json
+import importlib.resources as pkg_resources
+
+class Config:
+    def __init__(self, pcc):
+        from . import configs
+        with pkg_resources.path(configs, f"{pcc}.json") as configfile:
+            try:              
+                with open(str(configfile)) as js:
+                    self.config = json.load(js)
+                if(self.check()):
+                    print(f"Config {self.config['name']} loaded")
+                else:
+                    raise OSError(f"Config {self.config['name']} unavailable")
+            except FileNotFoundError:
+                raise FileNotFoundError(f"PCC config file {configfile} does not exist!")
+    
+    def check(self):
+        checks = ['avgr', 'fz', 't', 'combo', 'std', 'norm', 'chunk_idx', 'voxel_idx']
+        return all(list(map(os.path.exists, [self.config[key] for key in checks])))
+
+    def get(self, key):
+        return self.config[key]
+
+    def get_config(self):
+        return self.config
