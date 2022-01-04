c2e2c5acc6c126a8f8791c2aa64bb3abd910e834
William Drew <william.drew100@gmail.com>
2022-01-04 13:57:01 -0500

    Fix docstring

--- src/pfctoolkit/scripts/generate_pfc_combo_chunks.py
+++ src/pfctoolkit/scripts/generate_pfc_combo_chunks.py
@@ -1,6 +1,6 @@
 """usage: generate_pfc_combo_chunks.py [-h] [-b] -c -i -cs -o 
 
-Generate AvgR, AvgR_Fz, and T functional connectivity chunks for a single chunk.
+Generate Combo chunks for a single chunk.
 
 Arguments:
   -h, --help        show this help message and exit
@@ -16,8 +16,8 @@ Arguments:
   -cs, --conn-dir   Path to directory containing individual subject connectome
                     files.
 
-  -o, --output-dir  Path to output directory. */AvgR, */AvgR_Fz, and */T will be
-                    created in this directory if they do not already exist.
+  -o, --output-dir  Path to output directory. */Combo will be created in this 
+                    directory if they do not already exist.
 
 """
 import os
@@ -27,8 +27,8 @@ from pfctoolkit import processing, datasets
 from nilearn import image
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate AvgR, AvgR_Fz, and T"
-    " functional connectivity chunks for a single chunk.")
+    parser = argparse.ArgumentParser(description="Generate Combo chunks for a "
+                                                 "single chunk.")
 
     parser.add_argument("-b","--brain-mask", metavar='\b', help="Name of binary"
     " brain mask found in pfc.toolkits.datasets. Must be the same mask used to"
@@ -46,8 +46,8 @@ if __name__ == "__main__":
     required=True)
 
     parser.add_argument("-o", "--output-dir", metavar='\b', help="Path to "
-    "output directory. */AvgR, */AvgR_Fz, and */T will be created in this "
-    "directory if they do not already exist.", type=str, required=True)
+    "output directory. */Combo will be created in this directory if they do not"
+    " already exist.", type=str, required=True)
 
     args = parser.parse_args()
 
--- src/pfctoolkit/scripts/generate_pfc_weighted_masks.py
+++ src/pfctoolkit/scripts/generate_pfc_weighted_masks.py
@@ -1,23 +1,20 @@
-"""usage: generate_pfc_combo_chunks.py [-h] [-b] -c -i -cs -o 
+"""usage: generate_pfc_weighted_masks.py [-h] [-b] -cs [-n] -o 
 
-Generate AvgR, AvgR_Fz, and T functional connectivity chunks for a single chunk.
+Generate BOLD norm and stdev weighted masks for a precomputed connectome.
 
-Arguments:
+optional arguments:
   -h, --help        show this help message and exit
 
   -b, --brain-mask  Name of binary brain mask found in pfc.toolkits.datasets.
                     Must be the same mask used to generate the chunk index map.
                     Defaults to 'MNI152_T1_2mm_brain_mask_dil'
 
-  -c, --chunk-mask  Path to mask containing voxel-wise chunk labels.
-
-  -i, --chunk-idx   Index of chunk to process.
-
   -cs, --conn-dir   Path to directory containing individual subject connectome
                     files.
 
-  -o, --output-dir  Path to output directory. */AvgR, */AvgR_Fz, and */T will be
-                    created in this directory if they do not already exist.
+  -n, --conn-name   Name of connectome to use for naming mask files. Optional.
+
+  -o, --output-dir  Path to output directory.
 
 """
 import os
@@ -27,8 +24,8 @@ from pfctoolkit import processing, datasets
 from nilearn import image
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate AvgR, AvgR_Fz, and T"
-    " functional connectivity chunks for a single chunk.")
+    parser = argparse.ArgumentParser(description="Generate BOLD norm and stdev "
+    "weighted masks for a precomputed connectome.")
 
     parser.add_argument("-b","--brain-mask", metavar='\b', help="Name of binary"
     " brain mask found in pfc.toolkits.datasets. Must be the same mask used to"
@@ -39,11 +36,11 @@ if __name__ == "__main__":
     "directory containing individual subject connectome files.", type=str,
     required=True)
 
-    parser.add_argument("-n","--conn-name", metavar='\b', help="Name of connectome to use for naming mask files.", type=str, default="")
+    parser.add_argument("-n","--conn-name", metavar='\b', help="Name of "
+    "connectome to use for naming mask files.", type=str, default="")
 
     parser.add_argument("-o", "--output-dir", metavar='\b', help="Path to "
-    "output directory. */AvgR, */AvgR_Fz, and */T will be created in this "
-    "directory if they do not already exist.", type=str, required=True)
+    "output directory.", type=str, required=True)
 
     args = parser.parse_args()
 
