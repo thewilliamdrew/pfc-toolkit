c8f248d8cb13e0f83dd734fa110f92f4caaa7a8c
William Drew <william.drew100@gmail.com>
2022-07-06 18:08:07 -0400

    Update stuff

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LICENSE = "BSD License"
 
 setuptools.setup(
     name="pfc-toolkit",
-    version="0.0.10",
+    version="0.1.0",
     author="William Drew",
     author_email="william.drew100@gmail.com",
     description="The Precomputed Functional Connectome Toolkit",
